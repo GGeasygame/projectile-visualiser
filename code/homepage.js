b9d485daa9679e9d7e3986a527fd9a40a1cb8a5c
jonaskaufmann <jonas.kaufmann@sunrise.ch>
2021-12-03 17:02:05 +0100

    c

--- code/homepage.js
+++ code/homepage.js
@@ -11,15 +11,18 @@ function createTrajectory(that) {
 
     
     var g = 9.8;
+    var t = Math.sqrt(2)*that.velocity.value / g;
 
     ctx.beginPath();
     ctx.moveTo(0, 0);
 
 
-        var x = that.velocity.value*t*Math.cos(that.angle.value);
-        var y = that.velocity.value*t*Math.sin(that.angle.value);
-        var y = y-0.5*g*Math.pow(t, 2);
-        ctx.lineTo(x, y);
-    
+    for (var t2 = 0; t2 < t; t2 += 0.1) {
+        var x = that.velocity.value*t2*Math.cos(that.angle.value);
+        var y = that.velocity.value*t2*Math.sin(that.angle.value);
+        var y = y-0.5*g*Math.pow(t2, 2);
+        ctx.lineTo(x, -y);
+        // alert("X: " + x + " Y: " + -y + " T: " + t2);
+    }
     ctx.stroke();
 }
